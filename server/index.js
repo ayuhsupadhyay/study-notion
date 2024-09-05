9bfd4c06fe8eb8b163a74d899cf2401bc9e152d6
ayuhsupadhyay <ayushupadhyay345@gmail.com>
2024-09-05 21:48:31 +0530

    m

--- server/index.js
+++ server/index.js
@@ -24,12 +24,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Define CORS_ORIGIN directly here
-const CORS_ORIGIN = ["http://localhost:3000"];
+const CORS_ORIGIN = ["http://localhost:3000" ,"https://study-notion-blond.vercel.app/"];
 
 const corsOptions = {
   origin: CORS_ORIGIN,
   credentials: true,
-  maxAge: 14400,
+  
 };
 
 app.use(cors(corsOptions));
